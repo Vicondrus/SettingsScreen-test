ac4dde0c3d5ee9a8177cbc4f19c40951ed447e39
Victor <vicondrus@gmail.com>
2016-06-10 20:48:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/victor/settingsscreen/Settings.java
@@ -0,0 +1,12 @@
+package com.example.victor.settingsscreen;
+
+/**
+ * Created by Victor on 30.05.2016.
+ */
+public class Settings {
+
+    private String staus;
+    private int theme;
+    private int sounds;
+    private boolean notifications;
+}
